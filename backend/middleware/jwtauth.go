6003bda3da1962e468d3cf7cc479148f2eb672ec
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-09-21 23:07:47 -0400

    jwt

---
+++ backend/middleware/jwtauth.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"forum_api/helpers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := helpers.ValidateJWT(c)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
