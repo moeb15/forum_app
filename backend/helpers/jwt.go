43bda8559853508c0ffa49763ba8f38e78f05475
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-09-21 23:07:06 -0400

    jwt

---
+++ backend/helpers/jwt.go
@@ -0,0 +1,78 @@
+package helpers
+
+import (
+	"errors"
+	"fmt"
+	"forum_api/models"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var private_key = []byte(os.Getenv("JWT_PRIVATE_KEY"))
+
+func GenerateJWT(user models.User) (string, error) {
+	token_ttl, _ := strconv.Atoi(os.Getenv("JWT_TTL"))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":  user.ID,
+		"iat": time.Now().Unix(),
+		"eat": time.Now().Add(time.Second * time.Duration(token_ttl)).Unix(),
+	})
+	return token.SignedString(private_key)
+}
+
+func CurrentUser(c *gin.Context) (models.User, error) {
+	err := ValidateJWT(c)
+	if err != nil {
+		return models.User{}, err
+	}
+
+	token, _ := getToken(c)
+	claims, _ := token.Claims.(jwt.MapClaims)
+	userId := uint(claims["id"].(float64))
+
+	user, err := models.FindUserById(userId)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func ValidateJWT(c *gin.Context) error {
+	token, err := getToken(c)
+	if err != nil {
+		return err
+	}
+	_, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("Invalid token provided")
+}
+
+func getToken(c *gin.Context) (*jwt.Token, error) {
+	token_str := getTokenFromRequest(c)
+	token, err := jwt.Parse(token_str, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return private_key, nil
+	})
+
+	return token, err
+}
+
+func getTokenFromRequest(c *gin.Context) string {
+	bearer_token := c.Request.Header.Get("Authorization")
+	split_token := strings.Split(bearer_token, " ")
+	if len(split_token) == 2 {
+		return split_token[1]
+	}
+	return ""
+}
