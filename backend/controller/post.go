0f44e519fff39c66dc93499ad5df4dc0ad5fd83e
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-09-26 08:00:29 -0400

    created handler for updating posts

--- backend/controller/post.go
+++ backend/controller/post.go
@@ -99,3 +99,45 @@ func DeletePost(c *gin.Context) {
 
 	c.JSON(http.StatusNotFound, gin.H{"data": ""})
 }
+
+func UpdatePost(c *gin.Context) {
+	user, err := helpers.CurrentUser(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var updated_post models.Post
+	if err := c.ShouldBindJSON(&updated_post); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	post_id, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	post, err := models.FindPostById(uint(post_id))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if post.UserID != user.ID {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Unauthorized action"})
+		return
+	}
+
+	post.Content = updated_post.Content
+	post.Title = updated_post.Title
+
+	patched_post, err := post.Update()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": patched_post})
+}
