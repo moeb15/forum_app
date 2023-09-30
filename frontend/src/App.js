8466ade8fa1e18b5acc45d45b19697ebeb99e13e
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-09-30 07:21:07 -0400

    added route for posts page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Register from "./components/Register";
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import './styles/App.css'
 import Homepage from "./components/Homepage";
+import Postpage from "./components/Postpage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" Component={Login}/>
           <Route path="/register" Component={Register}/>
           <Route path="/home" Component={Homepage}/>
+          <Route path="/post" Component={Postpage}/>
         </Routes>
       </BrowserRouter>
     </div>
