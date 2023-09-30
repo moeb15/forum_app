db68578ea31ef41fee4f9214c2c3f26d31e96ecc
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-09-30 07:22:52 -0400

    redirects to post when user clicks expand button

--- frontend/src/components/ForumPost.js
+++ frontend/src/components/ForumPost.js
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function ForumPost({props}){
+    const navigate = useNavigate();
+
+    function expandPost(e){
+        e.preventDefault();
+        localStorage.setItem("current_post",props.ID);
+        navigate("/post")
+    }
 
     return(
         <div className="view-post">
+            <button onClick={expandPost}>Expand</button>
             <h3 id="post-title">{props.title}</h3>
             <h4 id="post-created">{props.CreatedAt}</h4>
         </div>
