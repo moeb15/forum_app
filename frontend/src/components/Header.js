e77f1aeb2c80bbd91917a99edb68502a7ebb5d05
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-03 11:16:19 -0400

    added button for logging out

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,25 @@
 import { AiOutlineHome } from "react-icons/ai";
 import { FaUserCircle } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
 import "../styles/Header.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Header(){
+    const navigate = useNavigate();
+
+    function logout(e){
+        e.preventDefault();
+        localStorage.setItem("token","");
+        navigate("/");
+    }
+    
     return(
         <div className="header">
+            <Link className="header-links"
+                  onClick={logout}>
+                <BiLogOut />
+            </Link>
             <Link className="header-links"
                   to="/user">
                 <FaUserCircle />
