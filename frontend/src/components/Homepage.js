4893693806ca63e40cc3e7254f1464ae62f1a52e
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-04 05:42:17 -0400

    added page navigation buttons

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -5,10 +5,11 @@ import Header from "./Header.js";
 
 function Homepage(){
     const [ posts,setPosts ] = useState({});
-    const getposts_url = "http://localhost:8000/api/posts/all"
+    const [ page,setPage ] = useState(1);
 
     useEffect(() => {
         const getposts = async() => {
+            const getposts_url = `http://localhost:8000/api/posts/all?page=${page}`;
             const response = await fetch(getposts_url,{
                 method:"GET",
                 headers:{
@@ -19,12 +20,17 @@ function Homepage(){
             const data = await response.json();
 
             if(data.hasOwnProperty("error")){
-                console.log("error");
+                alert(data.error);
+                return;
+            }else if(data.data.length === 0 && page !== 1){
+                alert("Last page");
+                setPage(page-1);
+                return;
             }
             setPosts(data.data);
         }
         getposts();
-    },[])
+    },[page])
 
     return(
         <>
@@ -34,6 +40,22 @@ function Homepage(){
                 {Array.from(posts).map((post, idx) => (
                     <ForumPost props={post} key={idx}/>
                 ))}
+            <div className="nav-btn">
+                    <button
+                        onClick={()=>{
+                            if(page === 1){
+                                setPage(1);
+                            }else{
+                                setPage(page-1)
+                            }
+                        }}>
+                        Prev
+                    </button>
+                    <button
+                        onClick={()=>{setPage(page+1)}}>
+                        Next
+                    </button>
+            </div>
             </div>
         </>
     )
