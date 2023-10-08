1161b0af86f5ea7e28601ede67669015c852b016
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-08 12:53:50 -0400

    translate height

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'height': 'height',
+      },
+    },
   },
   plugins: [],
 }
